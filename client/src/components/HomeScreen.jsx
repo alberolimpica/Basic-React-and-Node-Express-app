dd87de42622ee024c2fcde9c4d95c98f0d294b29
Sara Alberola Mahiques <sara.alberola@outlook.es>
2021-11-28 23:55:21 +0100

    Style home screen button

--- client/src/components/HomeScreen.jsx
+++ client/src/components/HomeScreen.jsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 
 function HomeScreen() {
     return <div className="phone">
-        <Link to="/phones"><button>Go check out our phones!</button></Link>
+        <Link to="/phones"><button style={{ width: 300, marginTop: 20, }}>Go check out our phones!</button>
+        </Link>
     </div>;
 }
 
